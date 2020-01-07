cc31cf10417fe24c4c0cb9e6f0cd4b4aa9894d5b
karpov-sv <karpov.sv@gmail.com>
2020-01-07 01:28:23 +0100

    Simple utility for removing WCS info from FITS header added

---
+++ clear_wcs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from astropy.io import fits
+import sys, glob
+
+wcs_keywords = ['WCSAXES', 'CRPIX1', 'CRPIX2', 'PC1_1', 'PC1_2', 'PC2_1', 'PC2_2', 'CDELT1', 'CDELT2', 'CUNIT1', 'CUNIT2', 'CTYPE1', 'CTYPE2', 'CRVAL1', 'CRVAL2', 'LONPOLE', 'LATPOLE', 'RADESYS', 'EQUINOX', 'B_ORDER', 'A_ORDER', 'BP_ORDER', 'AP_ORDER', 'CD1_1', 'CD2_1', 'CD1_2', 'CD2_2', 'COMMENT', 'HISTORY']
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+
+    parser = OptionParser()
+    (options,args) = parser.parse_args()
+
+    for filename in args:
+        print filename
+
+        fn = fits.open(filename, mode='update')
+        header = fn[0].header
+
+        for _ in header.keys():
+            if _ and (_[0] == '_' or _ in wcs_keywords):
+                header.remove(_, remove_all=True, ignore_missing=True)
+
+        fn.close()
