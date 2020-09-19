69b4bf3a16ed9bae94614171964ea2716e7543a3
karpov-sv <karpov.sv@gmail.com>
2020-09-19 15:30:58 +0200

    Update photometric conversions

--- db.py
+++ db.py
@@ -109,6 +109,13 @@ class DB:
             elif catalog in ['atlas']:
                 order = "ORDER BY g"
 
+        # if catalog == 'gaia':
+        #     if extra and type(extra) == str:
+        #         extra = [extra]
+        #     elif not extra:
+        #         extra = []
+        #     extra += ["lum > 0.3 AND lum < 30 AND bp_rp_excess > 1.0 + 0.015*bp_rp*bp_rp AND bp_rp_excess < 1.3 + 0.06*bp_rp*bp_rp "]
+
         if extra and type(extra) == list:
             extra_str = " AND " + " AND ".join(extra)
         elif extra and type(extra) == str:
@@ -140,13 +147,26 @@ class DB:
         # Add some computed fields to the table
         if catalog == 'pickles':
             # Pickles - has computed B, V, R and measured J, H, K, Bt, Vt
+            bt = table['bt']
+            vt = table['vt']
+
+            # http://www.aerith.net/astro/color_conversion.html
+            v = vt + 0.00097 - 0.1334 * (bt-vt) + 0.05486 * (bt-vt)**2 - 0.01998 * (bt-vt)**3
+            b = v + (bt-vt) - 0.007813 * (bt-vt) - 0.1489 * (bt-vt)**2 + 0.03384 * (bt-vt)**3
+
+            # quick and dirty calibration using Landolt (2009,2013)
+            # b = bt + 0.02927929 - 0.16965093*(bt-vt) - 0.07067606*(bt-vt)**2
+            # v = vt + 0.01082083 - 0.09096735*(bt-vt)
+
             table = np.lib.recfunctions.append_fields(table,
                         ['B', 'V', 'R', 'I', 'Berr', 'Verr', 'Rerr', 'Ierr'],
                         [
                             # B
-                            0.760*table['bt'] + 0.240*table['vt'], # B = 0.76*BT+0.24*VT
+                            b,
+                            # 0.760*table['bt'] + 0.240*table['vt'], # B = 0.76*BT+0.24*VT
                             # V
-                            table['vt'] - 0.090*(table['bt'] - table['vt']), # V = VT -0.090*(BT-VT)
+                            v,
+                            # table['vt'] - 0.090*(table['bt'] - table['vt']), # V = VT -0.090*(BT-VT)
                             # R
                             table['r'],
                             # I
@@ -163,7 +183,12 @@ class DB:
                         [np.double, np.double, np.double, np.double, np.double, np.double, np.double, np.double])
 
         elif catalog == 'apass':
-            # APASS - has measured B, V, g, r, i
+            # APASS - has measured B, V, g', r', i'
+            # g'r'i' to gri - http://classic.sdss.org/dr7/algorithms/jeg_photometric_eq_dr1.html
+            g = table['g'] + 0.060*(table['g']-table['r'] - 0.53)
+            r = table['r'] + 0.035*(table['r']-table['i'] - 0.21)
+            i = table['i'] + 0.041*(table['r']-table['i'] - 0.21)
+
             table = np.lib.recfunctions.append_fields(table,
                         ['B', 'V', 'R', 'I', 'Berr', 'Verr', 'Rerr', 'Ierr'],
                         [
@@ -172,9 +197,9 @@ class DB:
                             # V
                             table['v'],
                             # R
-                            table['r'] - 0.153*(table['r'] - table['i']) - 0.117, # R-r = (-0.153 +/- 0.003)*(r-i) - (0.117 +/- 0.003)
+                            table['r'] - 0.257*(r-i) - 0.152, # R-r = (-0.257 +/- 0.004)*(r-i) + (0.152 +/- 0.002)
                             # I
-                            table['v'] - 0.675*(table['g'] - table['i']) - 0.364, # V-I = (0.675 +/- 0.002)*(g-i)  + (0.364 +/- 0.002)
+                            table['v'] - 0.671*(g-i) - 0.359, # V-I = (0.671 +/- 0.002)*(g-i) + (0.359 +/- 0.002) if g-i <= 2.1
                             # Berr
                             table['berr'],
                             # Verr
@@ -188,18 +213,30 @@ class DB:
 
         elif catalog == 'atlas':
             # ATLAS-refcat2 - has measured Gaia, GaiaBP, GaiaRP, g, r, i, z (PanSTARRS ones!)
+            # Official transformations from https://arxiv.org/pdf/1203.0297.pdf
+            B = table['g'] + 0.212 + 0.556*(table['g'] - table['r']) + 0.034*(table['g'] - table['r'])**2
+            V = table['r'] + 0.005 + 0.462*(table['g'] - table['r']) + 0.013*(table['g'] - table['r'])**2
+            R = table['r'] - 0.137 - 0.108*(table['g'] - table['r']) - 0.029*(table['g'] - table['r'])**2
+            I = table['i'] - 0.366 - 0.136*(table['g'] - table['r']) - 0.018*(table['g'] - table['r'])**2
+
+            # Alternative transfromation from https://arxiv.org/pdf/1706.06147.pdf, Stetson, seems better with Landolt
+            B = table['g'] + 0.199 + 0.540*(table['g'] - table['r']) + 0.016*(table['g'] - table['r'])**2
+            V = table['g'] - 0.020 - 0.498*(table['g'] - table['r']) - 0.008*(table['g'] - table['r'])**2
+            R = table['r'] - 0.163 - 0.086*(table['g'] - table['r']) - 0.061*(table['g'] - table['r'])**2
+            I = table['i'] - 0.387 - 0.123*(table['g'] - table['r']) - 0.034*(table['g'] - table['r'])**2
+
             table = np.lib.recfunctions.append_fields(table,
                         ['B', 'V', 'R', 'I', 'Berr', 'Verr', 'Rerr', 'Ierr', 'zerr'],
                         [
-                            # Official transformations from https://arxiv.org/pdf/1203.0297.pdf
+                            # corrected by me for ~0.02 mag median difference with Landolt (1992) standards
                             # B
-                            table['g'] + 0.212 + 0.556*(table['g'] - table['r']) + 0.034*(table['g'] - table['r'])**2,
+                            B,
                             # V
-                            table['r'] + 0.005 + 0.462*(table['g'] - table['r']) + 0.013*(table['g'] - table['r'])**2,
+                            V,
                             # R
-                            table['r'] - 0.137 - 0.108*(table['g'] - table['r']) - 0.029*(table['g'] - table['r'])**2,
+                            R,
                             # I
-                            table['i'] - 0.366 - 0.136*(table['g'] - table['r']) - 0.018*(table['g'] - table['r'])**2,
+                            I,
                             # Berr
                             table['dg'],
                             # Verr
@@ -212,5 +249,44 @@ class DB:
                             table['dz'],
                         ],
                         [np.double, np.double, np.double, np.double, np.double, np.double, np.double, np.double, np.double])
+
+        elif catalog == 'gaia':
+            # Gaia DR2 data
+            # My simple fit using Landolt standards
+            pB = [ 0.00827462, -0.07061174,  0.37224837,  0.66911232,  0.01612951]
+            pV = [-0.05053524,  0.27906535, -0.34027179,  0.3705785 , -0.03900042]
+            pR = [-0.03439028,  0.18250704, -0.17043058, -0.22162674,  0.0165021 ]
+            pI = [ 0.02478455, -0.16225798,  0.46062943, -1.07182209,  0.09876639]
+
+            g = table['g']
+            bp_rp = table['bp'] - table['rp']
+
+            # https://www.cosmos.esa.int/web/gaia/dr2-known-issues#PhotometrySystematicEffectsAndResponseCurves
+            gcorr = g.copy()
+            gcorr[(g>2)&(g<6)] = -0.047344 + 1.16405*g[(g>2)&(g<6)] - 0.046799*g[(g>2)&(g<6)]**2 + 0.0035015*g[(g>2)&(g<6)]**3
+            gcorr[(g>6)&(g<16)] = g[(g>6)&(g<16)] - 0.0032*(g[(g>6)&(g<16)] - 6)
+            gcorr[g>16] = g[g>16] - 0.032
+            g = gcorr
+
+            B = g + np.polyval(pB, bp_rp)
+            V = g + np.polyval(pV, bp_rp)
+            R = g + np.polyval(pR, bp_rp)
+            I = g + np.polyval(pI, bp_rp)
+
+            err = np.sqrt(3)*table['dg']
+
+            table = np.lib.recfunctions.append_fields(table,
+                        ['B', 'V', 'R', 'I', 'Berr', 'Verr', 'Rerr', 'Ierr'],
+                        [
+                            B,
+                            V,
+                            R,
+                            I,
+                            err,
+                            err,
+                            err,
+                            err,
+                        ],
+                        [np.double, np.double, np.double, np.double, np.double, np.double, np.double, np.double])
 
         return table
